a8f746cd4d016fa653f38f8454dc13b59e43c9ec
dhole <dhole.me@gmail.com>
2020-05-18 20:39:57 +0800

    deploy

--- pt/apps/chat/chat.py
+++ pt/apps/chat/chat.py
@@ -34,10 +34,10 @@ def background_thread():
                       namespace='/test')
 
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html', async_mode=socketio.async_mode)
-#
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socketio.async_mode)
+
 
 @socketio.on('my_event', namespace='/test')
 def test_message(message):
